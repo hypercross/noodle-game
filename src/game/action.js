95b29fda9e0850e51971ede696cee2d74bcd25c6
hyper <hx.hyper@qq.com>
2020-11-05 08:54:34 +0800

    action code & test

---
+++ src/game/action.js
@@ -0,0 +1,120 @@
+import { AdhocEventTarget } from "./event";
+
+const onActiveChange = new Event('activeChanged');
+export function setActive(target, active) {
+  if (target.active == active) return;
+  target.active = active;
+  target.dispatchEvent(onActiveChange);
+}
+
+const onDisabledChange = new Event('disabledChanged');
+export function setDisabled(target, disabled) {
+  if (target.disabled == disabled) return;
+  target.disabled = disabled;
+  target.dispatchEvent(onDisabledChange);
+}
+
+export class Action extends AdhocEventTarget {
+  fromParams() {
+    const [name, match, run] = this.params;
+    this.name = name;
+    this.match = match;
+    this.run = run;
+  }
+
+  updateFromSelected(selected) {
+    const score = this.match(selected);
+    setActive(this, score > 0);
+    setDisabled(this, score < 0);
+    return score;
+  }
+}
+
+function trimSelected(selected, actions) {
+  let ok = false;
+  while (selected.length) {
+    for (const action of actions) {
+      if (ok) break;
+
+      const matched = action.match(selected);
+      if (matched >= 0)
+        ok = true;
+    }
+    if (!ok) {
+      selected.shift().active = false;
+    } else {
+      break;
+    }
+  }
+}
+
+function updateCandidate(selected, candidate, actions) {
+  if (candidate.active && !candidate.disabled) {
+    candidate.update();
+    return;
+  }
+
+  candidate.disabled = true;
+  const predicted = [...selected, candidate];
+  while (predicted.length) {
+    for (const action of actions) {
+      if (action.match(predicted) < 0) continue;
+      candidate.disabled = false;
+      break;
+    }
+    if (!candidate.disabled) break;
+    predicted.shift();
+  }
+  candidate.update();
+}
+
+export class ActionContext extends AdhocEventTarget {
+  selectables = [];
+  selected = [];
+  actions = [];
+
+  updateAll = () => {
+    const selected = this.selected;
+
+    trimSelected(selected, this.actions);
+    this.actions.forEach(action => action.updateFromSelected(selected));
+
+    for (const one of this.selectables) {
+      updateCandidate(selected, one, this.actions);
+    }
+  };
+
+  addSelectable(selectable) {
+    this.selectables.push(selectable);
+    const syncSelected = () => {
+      const i = this.selected.indexOf(selectable);
+      const active = !!selectable.active;
+      if (i < 0 && active) {
+        this.selected.push(selectable);
+      } else if (i >= 0 && !active) {
+        this.selected.splice(i, 1);
+      }
+
+      this.updateAll();
+    }
+
+    selectable['_onRemoveSelectable_' + this.id] = syncSelected;
+    selectable.addEventListener('activeChanged', syncSelected);
+  }
+
+  removeSelectable(selectable) {
+    const i = this.selectables.indexOf(selectable);
+    if (i < 0) return;
+
+    const cb = selectable['_onRemoveSelectable_' + this.id];
+    if (cb) {
+      selectable.removeEventListener('activeChanged', cb);
+    }
+
+    if (selectable.active) {
+      setActive(selectable, false);
+    }
+
+    this.selectables.splice(i, 1);
+  }
+}
---
+++ src/game/action.test.js
@@ -0,0 +1,65 @@
+import { ActionContext, Action, setActive, setDisabled } from "./action";
+import { AdhocEventTarget } from "./event";
+
+class Target extends AdhocEventTarget {
+    fromParams() {
+        const [quirk] = this.params;
+        this.quirk = quirk;
+    }
+}
+
+class SeqAction extends Action {
+    fromParams() {
+        const [sequence] = this.params;
+        this.sequence = sequence;
+        this.name = 'seq action ' + this.sequence;
+    }
+    match(selected) {
+        const seq = selected.map(s => s.quirk).join('');
+        return this.sequence == seq ? 1 : this.sequence.startsWith(seq) ? 0 : -1;
+    }
+    run() {
+        console.log('run ', this.sequence);
+    }
+}
+
+test("selecting", () => {
+    const ctx = new ActionContext();
+
+    const ab = new SeqAction('ab');
+    const bc = new SeqAction('bc');
+    ctx.actions.push(
+        ab, bc
+    );
+
+    const a = new Target('a');
+    const b = new Target('b');
+    const c = new Target('c');
+
+    ctx.addSelectable(a);
+    ctx.addSelectable(b);
+    ctx.addSelectable(c);
+
+    function summaryTarget(obj){
+        return `${obj.quirk || obj.sequence}${obj.active ? 'O' : obj.disabled ? 'X' : '-'}`;
+    }
+
+    function summary(){
+        return [a,b,c,ab,bc].map(summaryTarget).join('');
+    }
+
+    ctx.updateAll();
+    expect(summary()).toBe('a-b-cXab-bc-');
+
+    setActive(a, true);
+    expect(summary()).toBe('aOb-cXab-bcX');
+
+    setActive(b, true);
+    expect(summary()).toBe('a-bOc-abXbc-');
+
+    setActive(c, true);
+    expect(summary()).toBe('a-bOcOabXbcO');
+
+    setActive(a, true);
+    expect(summary()).toBe('aOb-cXab-bcX');
+});
