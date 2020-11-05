fe627028e508eb4f1344e8c48b05a1aa0276bf4d
hyper <hx.hyper@qq.com>
2020-11-05 11:23:05 +0800

    ignore & entry

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./kitchensink";
-import { makeGame } from "./setup";
+import { KitchenSink } from "./components/kitchensink";
+// import { makeGame } from "./setup";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<KitchenSink />, document.getElementById("root"));
 
-const game = makeGame(3);
-ReactDOM.render(<game.render />, document.getElementById("root"));
+// const game = makeGame(3);
+// ReactDOM.render(<game.render />, document.getElementById("root"));
